0a33f803401bc0b1519d961296d469f16e43cc35
vladislavshokorov <valsshokorov@gmail.com>
2022-12-12 13:47:55 +0600

    Update package.json

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <script setup>
-import { onMounted, watch } from "vue";
+import { watch } from "vue";
 import { useRoute } from "vue-router";
 import Toast from "primevue/toast";
 import SettingsSidebar from "@/components/SettingsSidebar.vue";
-import { useSettingsStore } from "@/stores/settings";
 
 const route = useRoute();
-const settingsStore = useSettingsStore();
-
-onMounted(() => {
-  settingsStore.setTheme();
-});
 
 watch(
   () => route.name,
   () => {
-    document.title = route.meta.title || "Shoqan Research";
+    document.title = route.meta.title || "School App";
   }
 );
 </script>
